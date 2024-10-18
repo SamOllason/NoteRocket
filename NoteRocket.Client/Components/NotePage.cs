aa3c4b0bb2a622eb0977aadedb9610cdba68cab1
Sam Ollason <sam.ollason@aplan.co.uk>
2024-10-18 10:13:36 +0100

    Adding button to go back to Notes page

--- NoteRocket.Client/Components/NotePage.cs
+++ NoteRocket.Client/Components/NotePage.cs
@@ -5,8 +5,6 @@ namespace NoteRocket.Client.Components;
 
 public partial class NotePage : ComponentBase
 {
-    // private Note note = new Note { Title = "Title 1", Body = "<b>NoteBody<b>" };
-
     [Parameter]
     public int Id { get; set; }
 
