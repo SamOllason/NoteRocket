b3b07410bc53408409231119a98f6779aec58361
Sam Ollason <sam.ollason@aplan.co.uk>
2024-10-18 09:29:09 +0100

    Moved NotePage logic into separate cs file

---
+++ NoteRocket.Client/Components/NotePage.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace NoteRocket.Client.Components;
+
+public partial class NotePage : ComponentBase
+{
+    // private Note note = new Note { Title = "Title 1", Body = "<b>NoteBody<b>" };
+
+    [Parameter]
+    public int Id { get; set; }
+
+
+
+};
