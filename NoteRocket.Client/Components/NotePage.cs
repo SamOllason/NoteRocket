4dbe038d63eb25bc3ba0f71c640a1944dc27bca1
Sam Ollason <sam.ollason@aplan.co.uk>
2024-10-18 10:38:52 +0100

    Adding form submission functionality to NotePage

--- NoteRocket.Client/Components/NotePage.cs
+++ NoteRocket.Client/Components/NotePage.cs
@@ -8,10 +8,20 @@ public partial class NotePage : ComponentBase
     [Parameter]
     public int Id { get; set; }
 
-    private Note? note;
+    // note: this needs to be a property in order to
+    // submit it back to the server after form submission
+    [SupplyParameterFromForm(FormName = "formNotePage")]
+    private Note? note { get; set; }
 
     protected override void OnParametersSet()
     {
-        note = NotesRepository.GetNoteById(Id);
+        // only retrieve data from the repo if note is null
+        // otherwise this will overwrite the form submissions!
+        note ??= NotesRepository.GetNoteById(Id);
     }
+    private void Submit()
+    {
+
+    }
+
 };
